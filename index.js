008b2d505f3dd734a50432993f4db2b6d39a74a6
Justin Hall <hall.justin.d@gmail.com>
2018-08-08 16:04:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ let tripID = 0;
 class Driver {
   constructor(name) {
     this.name = name;
-    this.id = driverId++;
+    this.id = driverID++;
     store.drivers.push(this);
   }
   trips() {
     return store.trips.filter(trip => {
-      return trip.driverId == this.id;
+      return trip.driverID == this.id;
     });
   }
   passengers() {
@@ -26,13 +26,13 @@ class Driver {
 class Passenger {
   constructor(name) {
     this.name = name;
-    this.id = passengerId++;
+    this.id = passengerID++;
     store.passengers.push(this);
   }
 
   trips() {
     return store.trips.filter(trip => {
-      return trip.passengerId == this.id;
+      return trip.passengerID == this.id;
     });
   }
   drivers() {
@@ -44,14 +44,14 @@ class Passenger {
 
 class Trip {
   constructor(driver, passenger) {
-    this.driverId = driver.id;
-    this.passengerId = passenger.id;
-    this.id = tripId++;
+    this.driverID = driver.id;
+    this.passengerID = passenger.id;
+    this.id = tripID++;
     store.trips.push(this);
   }
   driver() {
     return store.drivers.find(driver => {
-      return driver.id === this.driverId;
+      return driver.id === this.driverID;
     });
   }
   passenger() {
