b6a2cbb17526bbdb42eb5bf9aa521bb21a644ac5
Justin Hall <hall.justin.d@gmail.com>
2018-08-08 15:51:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,9 @@ class Passenger{
   }
   
   trips(){
-    return store.trips.filter(trip =>)
+    return store.trips.filter(trip => {
+      return trip.passengerID == this.id;
+    });
   }
   
   drivers(){
